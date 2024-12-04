fa1ee7744ac499674162eb187e50e5d640598d5a
Clive Stevens <clive.stevens@wayflyer.com>
2024-12-04 22:55:39 +0000

    tidy

--- aoc24/solutions/p4.py
+++ aoc24/solutions/p4.py
@@ -6,7 +6,7 @@ from aoc24.helpers.main import run_solution
 
 
 @dataclass(frozen=True)
-class Coord:
+class Position:
     i: int
     j: int
 
@@ -20,33 +20,35 @@ class Offset:
 Grid = list[str]
 
 
-def get_character(input: Grid, pos: Coord) -> str | None:
+def get_character(input: Grid, pos: Position) -> str | None:
     if not (0 <= pos.i < len(input)) or not (0 <= pos.j < len(input[0])):
+        # Out of bounds
         return None
     return input[pos.i][pos.j]
 
 
-def iter_grid(input: Grid) -> Generator[tuple[Coord, str], None, None]:
+def iter_grid(input: Grid) -> Generator[tuple[Position, str], None, None]:
     for i in range(len(input)):
         for j in range(len(input[i])):
-            yield Coord(i, j), input[i][j]
+            yield Position(i, j), input[i][j]
 
 
-def add_coord(pos: Coord, offset: Offset) -> Coord:
-    return Coord(pos.i + offset.i, pos.j + offset.j)
+def move_position(pos: Position, offset: Offset) -> Position:
+    return Position(pos.i + offset.i, pos.j + offset.j)
 
 
-def find_xmas(input: list[str], x: Coord, direction: Offset) -> bool:
+def find_xmas(input: list[str], x: Position, direction: Offset) -> bool:
+    # x is the position of an X. Walk in direction and see if MAS are found
     assert get_character(input, x) == "X"
     pos = x
     for c in "MAS":
-        pos = add_coord(pos, direction)
+        pos = move_position(pos, direction)
         if get_character(input, pos) != c:
             return False
     return True
 
 
-def same_row_or_col(a: Coord, b: Coord) -> bool:
+def same_row_or_col(a: Position, b: Position) -> bool:
     return a.i == b.i or a.j == b.j
 
 
@@ -69,17 +71,19 @@ def p4b(f: TextIO) -> int:
     diagonals = [Offset(*d) for d in itertools.product([1, -1], repeat=2)]
 
     input = f.read().split()
-    positions: dict[str, set[Coord]] = {c: set() for c in "MAS"}
+    # find MAS coordinates
+    positions: dict[str, set[Position]] = {c: set() for c in "MAS"}
     for pos, c in iter_grid(input):
         if c in positions:
             positions[c].add(pos)
 
     count = 0
     for a in positions["A"]:
-        surrounds = {add_coord(a, d) for d in diagonals}
+        surrounds = {move_position(a, d) for d in diagonals}
         count += (
             # diagonal adjacencies must be MMSS
             all(len(surrounds & positions[c]) == 2 for c in "MS")
+            # The Ms (or Ss) must be in the same row or col
             and same_row_or_col(*(surrounds & positions["M"]))
         )
     return count
