4bfcf8f1cd47a3f3380ca4247f6c26651d0be04e
Clive Stevens <clive.stevens@wayflyer.com>
2024-12-04 20:52:12 +0000

    wip

--- aoc24/solutions/p4.py
+++ aoc24/solutions/p4.py
@@ -1,26 +1,38 @@
 import itertools
-from typing import TextIO, cast
+from dataclasses import dataclass
+from typing import TextIO
 
 from aoc24.helpers.main import run_solution
 
-Coord = tuple[int, int]
-Offset = tuple[int, int]
+
+@dataclass
+class Coord:
+    i: int
+    j: int
+
+
+@dataclass
+class Offset:
+    i: int
+    j: int
+
+
 Grid = list[str]
 
 
 directions = [
-    cast(Offset, d) for d in itertools.product([0, 1, -1], repeat=2) if d != (0, 0)
+    Offset(*d) for d in itertools.product([0, 1, -1], repeat=2) if d != (0, 0)
 ]
 
 
 def get_character(input: Grid, pos: Coord) -> str | None:
-    if pos[0] >= len(input) or pos[1] >= len(input[0]) or any(p < 0 for p in pos):
+    if not (0 <= pos.i < len(input)) or not (0 <= pos.j < len(input[0])):
         return None
-    return input[pos[0]][pos[1]]
+    return input[pos.i][pos.j]
 
 
 def add_coord(pos: Coord, offset: Offset) -> Coord:
-    return pos[0] + offset[0], pos[1] + offset[1]
+    return Coord(pos.i + offset.i, pos.j + offset.j)
 
 
 def find_xmas(input: list[str], x: Coord, direction: Offset) -> bool:
@@ -38,7 +50,7 @@ def p4a(f: TextIO) -> int:
     count = 0
     for i in range(len(input)):
         for j in range(len(input[i])):
-            pos = i, j
+            pos = Coord(i, j)
             if get_character(input, pos) == "X":
                 for direction in directions:
                     count += find_xmas(input, pos, direction)
