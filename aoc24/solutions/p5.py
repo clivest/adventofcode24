eaf1cefd7ab13dc72c5905d55a1e764e8ba100cf
Clive Stevens <clive.stevens@wayflyer.com>
2024-12-05 20:19:10 +0000

    refactor

--- aoc24/solutions/p5.py
+++ aoc24/solutions/p5.py
@@ -1,60 +1,75 @@
-from typing import TextIO
+from typing import TextIO, Iterable
 
 from aoc24.helpers.main import run_solution
 
 
-def p5a(f: TextIO) -> int:
-    before_rules: dict[int, set[int]] = {}  # k must come before vs
-    lines = iter(f)
+Rules = dict[int, set[int]]
+
+
+def load_rules(lines: Iterable[str]) -> Rules:
+    # Loads rules, returning a dict where keys must come before the corresponding values
+    rules: dict[int, set[int]] = {}
     for l in lines:
         l = l.strip()
         if not l:
             break
         a, b = [int(c) for c in l.split("|")]
-        before_rules.setdefault(a, set()).add(b)
+        rules.setdefault(a, set()).add(b)
+    return rules
+
+
+def is_update_valid(update: list[int], rules: Rules) -> bool:
+    seen: set[int] = set()
+    for page in update:
+        if page in rules and (seen & rules[page]):
+            # If we've already seen an item which must come after `page`, the update is invalid
+            return False
+        seen.add(page)
+    return True
+
+
+def explode_rules(rules: Rules) -> None:
+    # Explode rules - if 1|2 and 2|3 add 1|3
+    for k, values in rules.items():
+        done: set[int] = set()
+        while done != values:
+            for v in values - done:
+                values |= rules.get(v, set())
+                done.add(v)
+        assert k not in values, f"Cycle detected: {k} {values}"
+
+
+def p5a(f: TextIO) -> int:
+    lines = iter(f)
+    rules = load_rules(lines)
 
     total = 0
     for l in lines:
         update = [int(c) for c in l.strip().split(",")]
-        seen: set[int] = set()
-        for page in update:
-            if page in before_rules and (seen & before_rules[page]):
-                break
-            seen.add(page)
-        else:
+        if is_update_valid(update, rules):
             # All rules followed
             total += update[len(update) // 2]
     return total
 
 
 def p5b(f: TextIO) -> int:
-    before_rules: dict[int, set[int]] = {}  # k must come before vs
     lines = iter(f)
-    for l in lines:
-        l = l.strip()
-        if not l:
-            break
-        a, b = [int(c) for c in l.split("|")]
-        before_rules.setdefault(a, set()).add(b)
+    before_rules = load_rules(lines)
 
     total = 0
     for l in lines:
         update = [int(c) for c in l.strip().split(",")]
-        seen: set[int] = set()
-        for page in update:
-            if page in before_rules and (seen & before_rules[page]):
-                break
-            seen.add(page)
-        else:
-            # All rules followed
-            continue
-        nums = set(update)
-        update = sorted(
-            update,
-            key=lambda i: len(nums & before_rules[i]) if i in before_rules else 0,
-        )
-        total += update[len(update) // 2]
-    print(total)
+        if not is_update_valid(update, before_rules):
+            # For there to be a unique ordering, we should be able to explode the ruleset (i.e. add rules for all
+            # transitive rules, e.g. if 1|3 and 3|4 then add 1|4) then sort by the number of rules. The page with the
+            # most rules must be before all of the other rules
+            # Note, there can be (and are!) cycles in the overall ruleset (e.g. 1|2 2|3 3|1) but there can't be any
+            # cycles in the partial ruleset for each update
+            nums = set(update)
+            update_rules = {k: before_rules[k] & nums for k in nums}
+            explode_rules(update_rules)
+            update = sorted(update, key=lambda i: len(update_rules[i]), reverse=True)
+            total += update[len(update) // 2]
     return total
 
 
