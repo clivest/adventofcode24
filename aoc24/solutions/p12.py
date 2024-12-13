f945fdf961041d7e86dcadd511a3458380457f6b
Clive Stevens <clive.stevens@wayflyer.com>
2024-12-13 17:19:13 +0000

    refactor

--- aoc24/solutions/p12.py
+++ aoc24/solutions/p12.py
@@ -7,7 +7,33 @@ from aoc24.helpers.main import run_solution
 directions = [Offset(1, 0), Offset(0, 1), Offset(-1, 0), Offset(0, -1)]
 
 
+def find_region(start: Position, plots: set[Position]) -> set[Position]:
+    # Find neighbours and their neighbours etc to form a region.
+    pos = start
+    region = {pos}
+    to_explore = {pos}
+    while to_explore:
+        p = to_explore.pop()
+        for d in directions:
+            neigh = move_position(p, d)
+            if neigh in plots and neigh not in region:
+                region.add(neigh)
+                to_explore.add(neigh)
+    return region
+
+
+def calc_perimeter(region: set[Position]) -> int:
+    # Each square contributes 4 - (num neighbour) sides to the perimeter
+    perm = 0
+    for p in region:
+        for d in directions:
+            neigh = move_position(p, d)
+            perm += neigh not in region
+    return perm
+
+
 def p12a(f: TextIO) -> int:
+    # Map plant to set of locations
     plots: dict[str, set[Position]] = {}
     for pos, c in iter_grid(f):
         plots.setdefault(c, set()).add(pos)
@@ -15,28 +41,10 @@ def p12a(f: TextIO) -> int:
     while plots:
         _, positions = plots.popitem()
         while positions:
-            pos = positions.pop()
-            region = {pos}
-            to_explore = {pos}
-            while to_explore:
-                p = to_explore.pop()
-                for d in directions:
-                    neigh = move_position(p, d)
-                    if neigh in positions:
-                        positions.discard(neigh)
-                        region.add(neigh)
-                        to_explore.add(neigh)
+            region = find_region(positions.pop(), positions)
+            positions -= region
             regions.append(region)
-    total = 0
-    for region in regions:
-        area = len(region)
-        perm = 0
-        for p in region:
-            for d in directions:
-                neigh = move_position(p, d)
-                perm += neigh not in region
-        total += area * perm
-    print(total)
+    total = sum(len(region) * calc_perimeter(region) for region in regions)
     return total
 
 
