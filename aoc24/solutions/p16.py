611055f8a65cacd07b46ae2678a1c9f6e8b582a9
Clive Stevens <clive.stevens@wayflyer.com>
2024-12-17 16:07:03 +0000

    wip

--- aoc24/solutions/p16.py
+++ aoc24/solutions/p16.py
@@ -1,7 +1,34 @@
+from dataclasses import dataclass
+from functools import total_ordering
+from heapq import heappush, heappop
 from typing import TextIO, Iterable
 
-from aoc24.helpers.grid import iter_grid, Offset, move_position, get_character, Position
+from aoc24.helpers.grid import (
+    iter_grid,
+    Offset,
+    move_position,
+    get_character,
+    Position,
+    Grid,
+)
 from aoc24.helpers.main import run_solution
+
+
+@dataclass
+@total_ordering
+class HeapItem:
+    cost: int
+    position: Position
+    move: Offset
+
+    def __lt__(self, other):
+        if isinstance(other, HeapItem):
+            return self.cost < other.cost
+        return NotImplemented
+
+    @property
+    def pos_move(self) -> tuple[Position, Offset]:
+        return self.position, self.move
 
 
 def rotations(move: Offset) -> Iterable[Offset]:
@@ -9,45 +36,37 @@ def rotations(move: Offset) -> Iterable[Offset]:
         yield Offset(ij if move.i == 0 else 0, ij if move.j == 0 else 0)
 
 
-def p16a(f: TextIO) -> int:
-    grid = [l.strip() for l in f]
-    start = None
-    end = None
+def find_start(grid: Grid) -> Position:
     for pos, c in iter_grid(grid):
         if c == "S":
-            assert start is None
-            start = pos
-        elif c == "E":
-            assert end is None
-            end = pos
-        if start and end:
-            break
-    assert start and end
-    costs = {(start, Offset(0, 1)): 0}
+            return pos
+    assert False, "No start found"
+
+
+def p16a(f: TextIO) -> int:
+    grid = [l.strip() for l in f]
+    start = find_start(grid)
+    costs_heap = [HeapItem(0, start, Offset(0, 1))]
     searched: set[tuple[Position, Offset]] = set()
-    min_path_cost = None
     while True:
-        to_search_pos, move = min(costs.keys() - searched, key=lambda c: costs[c])
-        cost = costs[(to_search_pos, move)]
-        if to_search_pos == end:
-            min_path_cost = cost
-            break
-        new_pos = move_position(to_search_pos, move)
-        new_costs = {}
+        while True:
+            current_state = heappop(costs_heap)
+            if current_state.pos_move not in searched:
+                break
+        if get_character(grid, current_state.position) == "E":
+            return current_state.cost
+        new_pos = move_position(current_state.position, current_state.move)
         if get_character(grid, new_pos) in [".", "E"]:
-            new_costs[(new_pos, move)] = cost + 1
-        for new_move in rotations(move):
-            new_costs[(to_search_pos, new_move)] = cost + 1000
-        costs.update(
-            {
-                k: min(costs[k], new_cost) if k in costs else new_cost
-                for k, new_cost in new_costs.items()
-            }
-        )
-        searched.add((to_search_pos, move))
-    assert min_path_cost
-    print(min_path_cost)
-    return min_path_cost
+            heappush(
+                costs_heap,
+                HeapItem(current_state.cost + 1, new_pos, current_state.move),
+            )
+        for new_move in rotations(current_state.move):
+            heappush(
+                costs_heap,
+                HeapItem(current_state.cost + 1000, current_state.position, new_move),
+            )
+        searched.add(current_state.pos_move)
 
 
 def p16b(f: TextIO) -> int:
