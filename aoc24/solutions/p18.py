94e47870c0342efbd55f9bbe5074cf5b416806dd
Clive Stevens <clive.stevens@wayflyer.com>
2024-12-22 22:56:58 +0000

    part_b

--- aoc24/solutions/p18.py
+++ aoc24/solutions/p18.py
@@ -6,10 +6,15 @@ from aoc24.helpers.grid import Position, Offset, move_position
 from aoc24.helpers.main import run_solution
 
 
+class NoPathFoundError(Exception):
+    pass
+
+
 @dataclass
 class HeapItem:
     pos: Position
     cost: int
+    path: set[Position]
 
     def __lt__(self, other):
         if isinstance(other, HeapItem):
@@ -20,17 +25,20 @@ class HeapItem:
 directions = [Offset(0, 1), Offset(1, 0), Offset(-1, 0), Offset(0, -1)]
 
 
-def min_path_len(
+def determine_min_path(
     start: Position, end: Position, obstacles: list[Position], grid_size: Offset
-) -> int:
+) -> set[Position]:
+    # p16.py also has a djikstra algorithm. TODO: refactor out a common implementation!
     searched: set[Position] = set()
-    paths: list[HeapItem] = [HeapItem(start, 0)]
+    paths: list[HeapItem] = [HeapItem(start, 0, set())]
     while True:
+        if not paths:
+            raise NoPathFoundError
         path = heapq.heappop(paths)
         if path.pos in searched:
             continue
         if path.pos == end:
-            return path.cost
+            return path.path | {end}
         for d in directions:
             pos = move_position(path.pos, d)
             if (
@@ -38,24 +46,47 @@ def min_path_len(
                 and 0 <= pos.i < grid_size.i
                 and 0 <= pos.j < grid_size.j
             ):
-                heapq.heappush(paths, HeapItem(pos, path.cost + 1))
+                heapq.heappush(
+                    paths, HeapItem(pos, path.cost + 1, path.path | {path.pos})
+                )
         searched.add(path.pos)
 
 
-def p18a(f: TextIO) -> int:
+def read_corrupt_positions(f: TextIO) -> list[Position]:
     corrupt = []
     for l in f:
         xy = l.strip().split(",")
         corrupt.append(Position(int(xy[1]), int(xy[0])))
-    corrupt = corrupt[:1024]
-    start = Position(0, 0)
-    end = Position(70, 70)
-    grid_size = Offset(71, 71)
-    return min_path_len(start, end, corrupt, grid_size)
+    return corrupt
 
 
-def p18b(f: TextIO) -> int:
-    return 1
+def p18a(f: TextIO) -> int:
+    corrupt = read_corrupt_positions(f)
+    min_path = determine_min_path(
+        Position(0, 0), Position(70, 70), corrupt[:1024], Offset(71, 71)
+    )
+    return len(min_path) - 1
+
+
+def p18b(f: TextIO, skip: int = 1024) -> str:
+    corrupt = read_corrupt_positions(f)
+    min_path = determine_min_path(
+        Position(0, 0), Position(70, 70), corrupt[:skip], Offset(71, 71)
+    )
+    for i, c in enumerate(corrupt[skip:]):
+        if c not in min_path:
+            continue
+        # else C would block current min path. Find a new min path
+        try:
+            min_path = determine_min_path(
+                Position(0, 0),
+                Position(70, 70),
+                corrupt[: skip + i + 1],
+                Offset(71, 71),
+            )
+        except NoPathFoundError:
+            return f"{c.j},{c.i}"
+    assert False, "No answer found"
 
 
 if __name__ == "__main__":
--- tests/solutions/test_p18.py
+++ tests/solutions/test_p18.py
@@ -2,7 +2,7 @@ from typing import TextIO
 
 import pytest
 
-from aoc24.solutions.p18 import p18a
+from aoc24.solutions.p18 import p18a, p18b
 
 
 @pytest.fixture
@@ -12,3 +12,8 @@ def day() -> int:
 
 def test_p18a_real_input(real_input: TextIO) -> None:
     assert p18a(real_input) == 438
+
+
+def test_p18b_real_input(real_input: TextIO) -> None:
+    # Skip a bit to speed up unit test
+    assert p18b(real_input, skip=2800) == "26,22"
