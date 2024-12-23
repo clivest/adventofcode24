1bdfa08b42f68c6e0908c8616744dc1737f1fee2
Clive Stevens <clive.stevens@wayflyer.com>
2024-12-23 00:30:10 +0000

    part_b

--- tests/solutions/test_p19.py
+++ tests/solutions/test_p19.py
@@ -15,4 +15,4 @@ def test_p19a_real_input(real_input: TextIO) -> None:
 
 
 def test_p19b_real_input(real_input: TextIO) -> None:
-    assert p19b(real_input) == 1
+    assert p19b(real_input) == 606411968721181
