27f9cb993f50b07ce2cc4fbb49333935aa3da8fe
Clive Stevens <clive.stevens@wayflyer.com>
2024-12-24 01:20:50 +0000

    refactor

--- aoc24/solutions/p21.py
+++ aoc24/solutions/p21.py
@@ -1,5 +1,5 @@
 from functools import cache
-from typing import TextIO, Iterable
+from typing import TextIO
 
 from aoc24.helpers.grid import Position
 from aoc24.helpers.main import run_solution
@@ -7,83 +7,58 @@ from aoc24.helpers.main import run_solution
 
 Keypad = dict[str, Position]
 
-dpad = {
-    "^": Position(0, 1),
-    "A": Position(0, 2),
-    "<": Position(1, 0),
-    "v": Position(1, 1),
-    ">": Position(1, 2),
-}
 
-
-def min_paths(code: str, pos: Position, keypad: Keypad, gap: Position) -> Iterable[str]:
+def min_paths(code: str, pos: Position, keypad: Keypad, gap: Position) -> set[str]:
     if not code:
-        yield ""
-        return
+        return {""}
+    possible_min_paths = set()
     for min_path in min_paths(code[1:], keypad[code[0]], keypad, gap):
         dest = keypad[code[0]]
         v = ("^" if dest.i < pos.i else "v") * abs(dest.i - pos.i)
         h = (">" if dest.j > pos.j else "<") * abs(dest.j - pos.j)
         if not (pos.j == gap.j and dest.i == gap.i):
-            yield v + h + "A" + min_path
-            if not v or not h:
-                continue
+            possible_min_paths.add(v + h + "A" + min_path)
         if not (pos.i == gap.i and dest.j == gap.j):
-            yield h + v + "A" + min_path
+            possible_min_paths.add(h + v + "A" + min_path)
+    return possible_min_paths
 
 
-def min_paths_r1(code: str) -> Iterable[str]:
-    keypad = {str(i): Position((9 - i) // 3, (i - 7) % 3) for i in range(1, 10)} | {
-        "0": Position(3, 1),
-        "A": Position(3, 2),
+def min_paths_r1(code: str) -> set[str]:
+    keypad = {s: Position(i // 3, i % 3) for i, s in enumerate("789456123.0A")}
+    return min_paths(code, keypad["A"], keypad, keypad["."])
+
+
+def calc_possible_dpad_moves() -> dict[tuple[str, str], set[str]]:
+    dpad = {s: Position(i // 3, i % 3) for i, s in enumerate(".^A<v>")}
+    mvs = {
+        (a, b): min_paths(b, pos, dpad, Position(0, 0))
+        for a, pos in dpad.items()
+        for b in dpad.keys()
     }
-    return min_paths(code, keypad["A"], keypad, Position(3, 0))
-
-
-def min_paths_rn(code: str) -> Iterable[str]:
-    return min_paths(code, dpad["A"], dpad, Position(0, 0))
-
-
-def move_count(dcode: str) -> int:
-    def mc(p1: Position, p2: Position) -> int:
-        return abs(p1.i - p2.i) + abs(p1.j - p2.j)
-
-    return sum(mc(dpad[c1], dpad[c2]) for c1, c2 in zip(dcode, dcode[1:]))
-
-
-def moves_() -> dict[tuple[str, str], set[str]]:
-    mvs = {}
-    gap = Position(0,0)
-    for a, pos in dpad.items():
-        for b, dest in dpad.items():
-            v = ("^" if dest.i < pos.i else "v") * abs(dest.i - pos.i)
-            h = (">" if dest.j > pos.j else "<") * abs(dest.j - pos.j)
-            mvs_i = set()
-            if not (pos.j == gap.j and dest.i == gap.i):
-                mvs_i.add(v + h + "A")
-            if not (pos.i == gap.i and dest.j == gap.j):
-                mvs_i.add(h + v + 'A')
-            mvs[(a,b)] = mvs_i
     return mvs
 
 
-moves = moves_()
+_possible_dpad_moves = calc_possible_dpad_moves()
+
 
 @cache
 def calc_move_len(start: str, target: str, depth: int) -> int:
-    seqs = moves[(start, target)]
+    seqs = _possible_dpad_moves[(start, target)]
     if depth == 0:
         return min(len(seq) for seq in seqs)
-    return min(sum(calc_move_len(s, t, depth - 1) for s, t in zip('A' + seq, seq)) for seq in seqs)
+    return min(
+        sum(calc_move_len(s, t, depth - 1) for s, t in zip("A" + seq, seq))
+        for seq in seqs
+    )
 
 
 def calc_path_len(path: str, depth: int) -> int:
-    return sum(calc_move_len(s, t, depth-1) for s, t in zip('A' + path, path))
+    return sum(calc_move_len(s, t, depth - 1) for s, t in zip("A" + path, path))
 
 
 def calc_complexity(code: str, robots: int) -> int:
-    paths = set(min_paths_r1(code))
-    return min(calc_path_len(p, robots-1) for p in paths) * int(code[:-1], base=10)
+    paths = min_paths_r1(code)
+    return min(calc_path_len(p, robots - 1) for p in paths) * int(code[:-1], base=10)
 
 
 def p21a(f: TextIO) -> int:
@@ -91,9 +66,7 @@ def p21a(f: TextIO) -> int:
 
 
 def p21b(f: TextIO) -> int:
-    c = sum(calc_complexity(l.strip(), 26) for l in f)
-    print(c)
-    return c
+    return sum(calc_complexity(l.strip(), 26) for l in f)
 
 
 if __name__ == "__main__":
