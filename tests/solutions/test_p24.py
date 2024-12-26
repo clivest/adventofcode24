c9f0ac4501a07dee7f4b2966f94d508ccd206578
Clive Stevens <clive.stevens@wayflyer.com>
2024-12-26 11:39:07 +0000

    wip

--- tests/solutions/test_p24.py
+++ tests/solutions/test_p24.py
@@ -15,4 +15,4 @@ def test_p24a_real_input(real_input: TextIO) -> None:
 
 
 def test_p24b_real_input(real_input: TextIO) -> None:
-    assert p24b(real_input) == 1
+    assert p24b(real_input) == "chv,jpj,kgj,rts,vvw,z07,z12,z26"
