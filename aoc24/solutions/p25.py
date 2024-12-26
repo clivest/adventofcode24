d9b3f70362396d1575f2f033dc720d465b709080
Clive Stevens <clive.stevens@wayflyer.com>
2024-12-26 12:30:53 +0000

    wip

--- aoc24/solutions/p25.py
+++ aoc24/solutions/p25.py
@@ -3,26 +3,29 @@ from typing import TextIO
 from aoc24.helpers.main import run_solution
 
 
+Heights = tuple[int, ...]
+
+
 def p25a(f: TextIO) -> int:
-    locks: set[tuple[int, ...]] = set()
-    keys: set[tuple[int, ...]] = set()
-    v: tuple[int, ...] = tuple()
-    is_lock: bool | None = None
+    locks: set[Heights] = set()
+    keys: set[Heights] = set()
+    v: Heights = tuple()
+    collection: set[Heights] | None = None
     for l in f:
         l = l.strip()
         if not l:
-            assert is_lock is not None
-            (locks if is_lock else keys).add(v)
+            assert collection is not None
+            collection.add(v)
             v = ()
-            is_lock = None
-        elif is_lock is None:
-            is_lock = all(c == "#" for c in l)
+            collection = None
+        elif collection is None:
+            collection = locks if all(c == "#" for c in l) else keys
             v = tuple(c == "#" for c in l)
         else:
             v = tuple(vi + (c == "#") for vi, c in zip(v, l))
     if v:
-        assert is_lock is not None
-        (locks if is_lock else keys).add(v)
+        assert collection is not None
+        collection.add(v)
     fit = sum(
         all(li + vi <= 7 for li, vi in zip(lock, key)) for lock in locks for key in keys
     )
